60fb3ceecc751a97b34e5aff2256496cd462562f
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-04-29 13:48:29 +0300

    finally

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector(".hero-list");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+const mySiema = new Siema({
+  selector: slider,
+  duration: 500,
+  loop: true,
+  easing: "ease-out",
+});
+
+function onNextClick() {
+  mySiema.next();
+}
+
+function onPrevClick() {
+  mySiema.prev();
+}
+
+next.addEventListener("click", onNextClick);
+prev.addEventListener("click", onPrevClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
